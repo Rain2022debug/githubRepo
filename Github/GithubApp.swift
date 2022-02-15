1adfb7de1914bed531e27ddb937256e4ecc94728
Jian Zhang <jizhang@thoughtworks.com>
2022-02-15 15:31:15 +0800

    Initial Commit

---
+++ Github/GithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubApp.swift
+//  Github
+//
+//  Created by Jian on 2022/2/15.
+//
+
+import SwiftUI
+
+@main
+struct GithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
